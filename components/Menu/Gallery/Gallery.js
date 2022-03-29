edf7afca31a2efc64697addec2b14771fe72fdd4
Avram Adrian-Constantin <56772022+kkrogmerio@users.noreply.github.com>
2022-03-29 15:41:19 +0300

    .

--- components/Menu/Gallery/Gallery.js
+++ components/Menu/Gallery/Gallery.js
@@ -1,17 +1,29 @@
 import React, { Component } from 'react';
-import { View, Text,StyleSheet } from 'react-native';
+import { View, Text,StyleSheet,Modal } from 'react-native';
 import MenuBackgroundImage from '../../UI/MenuBackgroundImage';
 export default class Gallery extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      isModalVisible: false,
+      displayPhotoModal:undefined
     };
   }
+  // componentWillReceiveProps(nextProps) {
+  //   console.log(this.props.navigation.state.params.linkToPhoto,nextProps.navigation.state.params.linkToPhoto);
+  // }
 
   render() {
-    console.log(this.props.navigation.state.params.linkToPhoto,"UAAAU")
+      // console.log(this.state.displayPhotoModal," ",this.props.navigation.state.params.linkToPhoto)
     return (
       <View style={{flex:1}}>
+    {this.props.navigation.state.params&&this.props.navigation.state.params.linkToPhoto&&    <Modal isVisible={true} transparent={true}>
+        <View style={{ flex: 1 }}>
+          <Text>{this.props.navigation.state.params.linkToPhoto}!</Text>
+
+          
+        </View>
+      </Modal>}
         <MenuBackgroundImage/>
       </View>
     );
