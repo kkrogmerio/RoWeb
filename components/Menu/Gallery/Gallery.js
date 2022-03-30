618092439ecf4441b92b0fb0f37a9f69ffd3ff87
Avram Adrian-Constantin <56772022+kkrogmerio@users.noreply.github.com>
2022-03-30 09:58:02 +0300

    About restaurant UI Fully implemented

--- components/Menu/Gallery/Gallery.js
+++ components/Menu/Gallery/Gallery.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
-import { View, Text,StyleSheet,Modal } from 'react-native';
+import { View, Text,StyleSheet,Image,Dimensions } from 'react-native';
 import MenuBackgroundImage from '../../UI/MenuBackgroundImage';
+import Modal from "react-native-modal";
 export default class Gallery extends Component {
   constructor(props) {
     super(props);
@@ -9,22 +10,21 @@ export default class Gallery extends Component {
       displayPhotoModal:undefined
     };
   }
-  // componentWillReceiveProps(nextProps) {
-  //   console.log(this.props.navigation.state.params.linkToPhoto,nextProps.navigation.state.params.linkToPhoto);
-  // }
+  componentWillReceiveProps(nextProps) {
+    console.log(this.props.navigation.state.params.linkToPhoto,nextProps.navigation.state.params.linkToPhoto);
+  }
 
   render() {
       // console.log(this.state.displayPhotoModal," ",this.props.navigation.state.params.linkToPhoto)
     return (
-      <View style={{flex:1}}>
-    {this.props.navigation.state.params&&this.props.navigation.state.params.linkToPhoto&&    <Modal isVisible={true} transparent={true}>
-        <View style={{ flex: 1 }}>
-          <Text>{this.props.navigation.state.params.linkToPhoto}!</Text>
-
-          
+      <View style={{flex:1,backgroundColor:'red'}}>
+    {this.props.navigation.state.params&&this.props.navigation.state.params.linkToPhoto&&   <Modal   isVisible={true}>
+        <View style={{marginLeft:90,alignItems: 'center',justifyContent: 'center'}}>
+          <Image style={{width:60,height:60,resizeMode: 'contain',marginBottom:20}} source={require('../../../assets/icons/icon_close.png')}/>
+          <Image style={{width:Dimensions.get('window').width/1.2,height:Dimensions.get('window').height/1.22,resizeMode: 'contain'}} source={{uri:this.props.navigation.state.params.linkToPhoto}}/>
         </View>
       </Modal>}
-        <MenuBackgroundImage/>
+        {/* <MenuBackgroundImage/> */}
       </View>
     );
   }
